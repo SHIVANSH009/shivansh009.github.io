4a0cbf5c88c38bbe11dba0d266d43459e20d9e3e
shivansh <shivanshsingh009@gmail.com>
2020-08-15 09:13:17 +0530

    updated db env.

--- database/mongoose.js
+++ database/mongoose.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
 
-mongoose.connect('mongodb://127.0.0.1:27017/angular',{ useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false})
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/angular',{ useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false})
     .then(()=>console.log('database created'))
     .catch((error)=>console.log(error))
 
