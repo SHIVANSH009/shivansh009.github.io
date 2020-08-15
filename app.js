367d20acf524da336350c3f36b1e235fb20fa69e
shivansh <shivanshsingh009@gmail.com>
2020-08-15 11:56:35 +0530

    Added backend connection with Frontend

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const bodyParser = require('body-parser');
 const mongoose = require('./database/mongoose');
 const StudentRoutes = require('./routes/auth.routes');
 const cors = require('cors');
+const path = require('path');
 const TeacherRoutes = require('./routes/teacher.routes');
 const FileRoutes = require('./routes/file.routes');
 
@@ -14,6 +15,12 @@ app.use(bodyParser.urlencoded({extended: false }));
 // Serve static resources
 app.use('/public', express.static('public'));
 app.use(cors());
+
+// Application Connection with Frontend
+app.use(express.static(__dirname + "/dist/mean-app-on-heroku"));
+app.get('/*',function(req,res){
+    res.sendFile(path.join(__dirname + "/dist/mean-app-on-heroku/index.html"));
+})
 
 app.use('/',FileRoutes);
 app.use('/api',StudentRoutes);
