77c9a091fc4080eb57a6494a57dd83472c377cdb
SHIVANSH009 <36429778+SHIVANSH009@users.noreply.github.com>
2020-12-25 00:01:38 +0530

    Update jenkins req

--- app-jenkins.js
+++ app-jenkins.js
@@ -17,10 +17,10 @@ app.use('/public', express.static('public'));
 app.use(cors());
 
 // Application Connection with Frontend
-app.use(express.static(__dirname + "/src"));
-app.get('/*',function(req,res){
-    res.sendFile(path.join(__dirname + "/src/index.html"));
-})
+//app.use(express.static(__dirname + "/src"));
+//app.get('/*',function(req,res){
+//    res.sendFile(path.join(__dirname + "/src/index.html"));
+//})
 
 app.use('/',FileRoutes);
 app.use('/api',StudentRoutes);
